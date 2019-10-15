208fa7c1dd4d322aeda9cb4d95fdf338350ea9e3
Thales <thales@Thaless-MacBook-Pro.local>
2019-10-15 20:39:44 +0800

    Complete

---
+++ Main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+latex My_Report_of_Operator_Theory.tex
+
+bibtex My_Report_of_Operator_Theory
+
+latex My_Report_of_Operator_Theory.tex
+
+latex My_Report_of_Operator_Theory.tex
+
+dvipdf My_Report_of_Operator_Theory.dvi My_Report_of_Operator_Theory.pdf
--- la.sh
+++
@@ -1,7 +0,0 @@
-#！/usr/bin/bash
-
-latex $1.tex
-bibtex $1.aux
-latex $1.tex
-bibtex $1.aux
-dvipdf $1.dvi $1.pdf
